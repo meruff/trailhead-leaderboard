99749de37fcc8b13de099f9bde39fde7e0d92e0a
meruff <mathew.ruff@gmail.com>
2021-04-23 09:42:35 -0700

    Fix missing date earned for certifications by formatting dateCompleted to match ISO required format of lightning-formatted-date-time

--- force-app/main/default/lwc/leaderboardCertification/leaderboardCertification.js
+++ force-app/main/default/lwc/leaderboardCertification/leaderboardCertification.js
@@ -3,6 +3,10 @@ import { LightningElement, api } from "lwc";
 export default class LeaderboardCertification extends LightningElement {
     @api certification;
 
+    get dateCompleted() {
+        return new Date(this.certification.dateCompleted).toISOString();
+    }
+
     get showEndDate() {
         return this.certification.certificationStatus === "EXPIRED" && this.certification.dateExpired;
     }
